c0d3208ce5dd07c588807889c43e09e39a12b99e
pltnw <113547510+pltnw@users.noreply.github.com>
2022-12-10 16:15:34 +0100

    Interface setup

--- TrafficLightPro/ContentView.swift
+++ TrafficLightPro/ContentView.swift
@@ -9,14 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack{
+            ZStack {
+                Rectangle()
+                    .foregroundColor(.gray)
+                    .frame(width: 200, height: 500)
+                    .cornerRadius(30)
+                
+                
+                VStack{
+                    Circle()
+                        .foregroundColor(.red)
+                        .frame(width: 120, height: 120)
+                        .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                        .shadow(radius: 10)
+                        .padding()
+                    
+                    Circle()
+                        .foregroundColor(.yellow)
+                        .frame(width: 120, height: 120)
+                        .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                        .shadow(radius: 10)
+                        .padding()
+                    
+                    Circle()
+                        .foregroundColor(.green)
+                        .frame(width: 120, height: 120)
+                        .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                        .shadow(radius: 10)
+                        .padding()
+                
+                }
+            }
+            ZStack{
+                Rectangle()
+                    .foregroundColor(.gray)
+                    .frame(width: 50, height: 200)
+                
+                Rectangle()
+                    .foregroundColor(.gray)
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(25)
+                
+                Circle()
+                    .foregroundColor(.red)
+                    .frame(width: 75, height: 75)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                    .shadow(radius: 10)
+                
+            }
         }
-        .padding()
     }
+}
+
+func someAction() {
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
