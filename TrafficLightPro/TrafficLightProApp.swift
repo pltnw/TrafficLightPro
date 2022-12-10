5aeae3771ac5eaef841fca91de69e119185b609b
pltnw <113547510+pltnw@users.noreply.github.com>
2022-12-10 14:32:59 +0100

    Initial Commit

---
+++ TrafficLightPro/TrafficLightProApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightProApp.swift
+//  TrafficLightPro
+//
+//  Created by Екатерина Платонова on 10.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
